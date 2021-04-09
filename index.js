195991c767e087ad6def4a64759adcfee7621693
DIKA DWI SEPTIAN <82138046+Dika-hub@users.noreply.github.com>
2021-04-09 14:14:08 +0700

    DW1GARABOT.JS
    Rwt

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'DW1GARABOT'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:DIKA\n' // Nama kamu
+            + 'ORG:DW1GARABOT;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6289688261114:+62 896-8826-1114\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
